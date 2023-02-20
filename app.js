d9a1ba5ae38583bfabbf17863569400ba62d312e
HARSH24-1997 <rajput24.harsh@gmail.com>
2023-02-20 23:00:09 +0530

    1

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors({
   credentials: true,
-  origin: ["http://localhost:4200"],
+  origin: ["https://thepeopleorderdash.netlify.app"],
 }));
 
 app.use('/', indexRouter);
